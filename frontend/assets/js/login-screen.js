c1b3ccc1c008d14372ee91d854f8c6747794392d
rennedenner <rennedenner@icloud.com>
2016-11-17 11:01:16 -0200

    Main page created

--- frontend/assets/js/login-screen.js
+++ frontend/assets/js/login-screen.js
@@ -6,5 +6,17 @@
 			window.location = '#/default';
 		}
 	}]);
-    
+
+
+	// controllers of main page
+	angular.module('app.login').controller('navCtrl', function($scope) {
+	  $scope.nav = {
+	    navItems: ['HOME', 'CREATE DISCIPLINE', 'LIST DISCIPLINE', 'ABOUT CP'],
+	    selectedIndex: 0,
+	    navClick: function($index) {
+	      $scope.nav.selectedIndex = $index;
+	    }
+	  };
+	})
+
 })();
