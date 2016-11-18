f35814f047f5238b2c93aa6d92a1372295683c94
braulley <juniorbraulley@gmail.com>
2016-11-17 22:01:01 -0200

    start of login request

--- frontend/assets/js/login-screen.js
+++ frontend/assets/js/login-screen.js
@@ -1,27 +1,38 @@
 (function () {
 	'use strict';
-	angular.module('app.login').controller('LoginCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+	angular.module('app.login').controller('LoginCtrl', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
 
 		$scope.doLogin = function () {
-			window.location = '#/default';
+			var datajson = {
+				 usuario:$scope.loginData.user,
+				 senha: $scope.loginData.senha
+				} 
+			$http.post('../backend/Classpictures/public/iindex.php/url-da-requisicao', datajson)
+				.success(function(data, status, headers, config) {
+  				// sucesso! 
+  				// data agora contém o que foi retornado pelo servidor
+  				})
+				.error(function(data, status, headers, config) {
+		  				// erro!
+		  				// você pode verificar o parâmetro "status" para ver o código HTTP do erro
+				});
+			// window.location = '#/default';
 		}
+
+
+		$scope.loginData = {};
 	}]);
 
 
-	// controllers of main page
-	angular.module('app.login').controller('navCtrl', function($scope) {
-	  $scope.nav = {
-	    navItems: ['CREATE DISCIPLINE', 'LIST DISCIPLINE', 'ABOUT CP'],
-	    selectedIndex: 0,
-	    navClick: function($index) {
-	      $scope.nav.selectedIndex = $index;
-	    }
-	  };
-	});
-
-	// 
-	angular.module('app.login').controller('navbar', function($scope) {
-	  $scope.usuario = 'Fulano da Silva';
-	});
+	// // controllers of main page
+	// angular.module('app.login').controller('navCtrl', function($scope) {
+	//   $scope.nav = {
+	//     navItems: ['CREATE DISCIPLINE', 'LIST DISCIPLINE', 'ABOUT CP'],
+	//     selectedIndex: 0,
+	//     navClick: function($index) {
+	//       $scope.nav.selectedIndex = $index;
+	//     }
+	//   };
+	// });
 
 })();
